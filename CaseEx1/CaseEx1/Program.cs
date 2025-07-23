0a83314ba775e77527e8b39f610ba4e0fc2ce8e3
yaminiparuchuri16 <paruchuriyamini16@gmail.com>
2025-07-23 04:13:24 -0700

    CaseEx1

---
+++ CaseEx1/CaseEx1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoApplication
+{
+    internal class CaseEx1
+    {
+        public void Check(char choice)
+        {
+            switch (choice)
+            {
+                case 'a':
+                case 'A':
+                case '1':
+                    Console.WriteLine("Hi I am Rajesh");
+                    break;
+                case 'b':
+                case 'B':
+                case '2':
+                    Console.WriteLine("Hi I am Yamini...");
+                    break;
+                case 'c':
+                case 'C':
+                case '3':
+                    Console.WriteLine("Hi I am Sreeja...");
+                    break;
+                default:
+                    Console.WriteLine("Invalid Choice");
+                    break;
+            }
+        }
+        static void Main()
+        {
+            char choice;
+            Console.WriteLine("Enter Your Choice  ");
+            choice = Convert.ToChar(Console.ReadLine());
+            CaseEx1 obj = new CaseEx1();
+            obj.Check(choice);
+        }
+    }
+}
