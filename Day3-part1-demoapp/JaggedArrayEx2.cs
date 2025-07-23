ad05bcd94b8548c0c648b0c2a1f11a544dc888a4
yaminiparuchuri16 <paruchuriyamini16@gmail.com>
2025-07-23 10:06:13 -0700

    Day3-part1-demoapp

---
+++ Day3-part1-demoapp/JaggedArrayEx2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoApp
+{
+    internal class JaggedArrayEx2
+    {
+        static void Main()
+        {
+            int[][] jaggedArray = new int[2][];
+
+            int[] x = new int[] { 12, 5, 22 };
+            int[] y = new int[] { 55, 11, 124 };
+
+            jaggedArray[0] = x;
+            jaggedArray[1] = y;
+
+            for(int i=0; i<jaggedArray.Length; i++)
+            {
+                for(int j=0;j<jaggedArray[i].Length;j++)
+                {
+                    Console.Write(jaggedArray[i][j] + "    ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Day3-part1-demoapp/Matrix.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoApp
+{
+    internal class Matrix
+    {
+        static void Main()
+        {
+            int[,] x = new int[2, 3]
+            {
+                {1, 2, 3},
+                {5,2,8 }
+            };
+
+            for (int i = 0; i < x.GetLength(0); i++)
+            {
+                for(int j=0;j<x.GetLength(1);j++)
+                {
+                    Console.Write(x[i,j] + "  ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
