629157e00769b4c459cf0b5a848bb94c6ed626f1
yaminiparuchuri16 <paruchuriyamini16@gmail.com>
2025-07-23 22:52:57 -0700

    Day3-Customarray

---
+++ Day3-Customarray/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomArray
+{
+    internal class Student
+    {
+        int sid;
+        string sname;
+        double cgpa;
+        string city;
+
+        public Student() { }
+
+        public Student(int sid, string sname, double cgpa, string city)
+        {
+            this.sid = sid;
+            this.sname = sname;
+            this.cgpa = cgpa;
+            this.city = city;
+        }
+
+        public override string ToString()
+        {
+            return "Sid " + sid + " Name " + sname + " Cgp  " + cgpa + " City " + city;
+        }
+    }
+}
