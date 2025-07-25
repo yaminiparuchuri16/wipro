c946ec4c1e1be0d44f81e2751766475d9f79a0a2
yaminiparuchuri16 <paruchuriyamini16@gmail.com>
2025-07-24 23:42:04 -0700

    Day4-part2-FliterException

---
+++ Day4-part2-FliterException/FilterExampleMain.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    internal class FilterExampleMain
+    {
+        static void RaiseSeverity(string severity)
+        {
+            if (severity.Equals("low"))
+            {
+                throw new FilterExampleException("low severity you can leave for time being...");
+            } else if (severity.Equals("medium"))
+            {
+                throw new FilterExampleException("medium severity you need to try to fix without leaving...");
+            } else if (severity.Equals("high"))
+            {
+                throw new FilterExampleException("high severity You need to fix on high priority...");
+            } else if (severity.Equals("critical"))
+            {
+                throw new FilterExampleException("critical error as fix first as stop all other works...");
+            } else
+            {
+                throw new FilterExampleException("No Error Occurred...");
+            }
+        }
+        static void Main()
+        {
+            string severity;
+            Console.WriteLine("Enter the Severity (low/medium/high/critical)  ");
+            severity = Console.ReadLine();
+            try
+            {
+                RaiseSeverity(severity);
+            }
+            catch(FilterExampleException e) when (e.Message.Contains("low"))
+            {
+                Console.WriteLine(e.Message);
+            } 
+            catch(FilterExampleException e) when (e.Message.Contains("medium"))
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch(FilterExampleException e) when (e.Message.Contains("high"))
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch(FilterExampleException e) when (e.Message.Contains("critical"))
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch(FilterExampleException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+             catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
---
+++ Day4-part2-FliterException/MileStoneExam.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    internal class MileStoneExam
+    {
+        static void ShowMileStoneInfo(string examCode)
+        {
+            if (examCode.Equals("M1"))
+            {
+                throw new MileStoneException("MileStone1 Contains Core Concepts of .NET");
+            } else if (examCode.Equals("M2"))
+            {
+                throw new MileStoneException("MileStone2 Contains Asp.net Rajor Core Concepts...");
+            } else if (examCode.Equals("M3"))
+            {
+                throw new MileStoneException("MileStone3 Contains Advanced Core Concepts with Db...");
+            } else if (examCode.Equals("M4"))
+            {
+                throw new MileStoneException("MileStone4 Contains Advanced React Framework  ");
+            } else if (examCode.Equals("Project"))
+            {
+                throw new MileStoneException("Project Capstone to be Done Last...");
+            } else
+            {
+                throw new MileStoneException("No Error Occurred...");
+            }
+        }
+        static void Main()
+        {
+            string examCode;
+            Console.WriteLine("Enter Exam Code (M1, M2, M3, M4, Project)  ");
+            examCode = Console.ReadLine();
+            try
+            {
+                ShowMileStoneInfo(examCode);
+            }
+            catch(MileStoneException e) when (e.Message.Contains("MileStone1"))
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (MileStoneException e) when (e.Message.Contains("MileStone2"))
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (MileStoneException e) when (e.Message.Contains("MileStone3"))
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (MileStoneException e) when (e.Message.Contains("MileStone4"))
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (MileStoneException e) when (e.Message.Contains("Project"))
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
